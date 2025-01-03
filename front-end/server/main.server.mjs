0cfa119192cf2147bc8621fe37fae36c515175d0
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2025-01-03 17:48:22 +0100

    fourth commit

--- front-end/server/main.server.mjs
+++ front-end/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-JEKRL6NR.mjs";import"./chunk-LIR2YFIB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-CUA2I3MR.mjs";import"./chunk-LIR2YFIB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
