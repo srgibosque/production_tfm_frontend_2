f443de09669d0822b13244e0b05c1946c1726686
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2025-01-03 15:02:15 +0100

    second commit

--- front-end/server/main.server.mjs
+++ front-end/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WP265SLQ.mjs";import"./chunk-LIR2YFIB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-P64UOTYY.mjs";import"./chunk-LIR2YFIB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
